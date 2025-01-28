08baa5fc800ce71b8821d777b0f64b12504e6e42
AbdAlrhman Nasser <AbdAlrhmanNasser10>
2025-01-28 14:14:22 +0200

    finalize prefix in dispatch method

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,17 @@
 use App\Http\Controllers\HomeController;
 use Ililuminates\Router\Route;
 
-Route::get('/', HomeController::class, 'index', []);
+// Route::get('/', HomeController::class, 'index', []);
 Route::get('/', function () {
     return "Welcome to index page";
 }, ['simple,user']);
-Route::get('/about', HomeController::class, 'about');
-// Route::get('/article/{id}', HomeController::class, 'article', []);
-Route::get('/article/{id}/{name}', function ($id, $name) {
-    return $id . " " . $name;
+
+Route::group(['prefix'=>'site'] , function(){
+
+    Route::get('/about', HomeController::class, 'about');
+    // Route::get('/article/{id}', HomeController::class, 'article', []);
+    Route::get('/article/{id}/{name}', function ($id, $name) {
+        return $id . " " . $name;
+    });
 });
+
